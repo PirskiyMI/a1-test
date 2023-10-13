5392c5d3ff498eab5b84e27c011a452c8c793caf
Pirskiy Maxim <pirskiymi98@gmail.com>
2023-10-13 12:20:31 +0300

    fixed Details

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -4,23 +4,19 @@ import Table from '../Table/Table';
 import Glow from '../shared/Glow/Glow';
 import MyButton from './../UI/my-button/MyButton';
 
+import vitoImage from './assets/images/details-section-vito.svg';
+import boxImage from './assets/images/details-section-box.png';
+
 const Details: FC = () => {
    return (
       <section className={styles.details}>
          <div className={styles.details__container}>
-            <img
-               className={styles.details__image}
-               src="./src/components/Details/assets/images/details-section-vito.svg"
-               alt="Вито"
-            />
+            <img className={styles.details__image} src={vitoImage} alt="Вито" />
             <div className={styles.details__info}>
                <h2 className={styles.details__title}>набор «ПОСЫЛКА С КОНТРАБАНДОЙ»</h2>
                <Table />
                <div className={styles.details__order}>
-                  <img
-                     src="./src/components/Details/assets/images/details-section-box.png"
-                     alt="Коллекционное издание"
-                  />
+                  <img src={boxImage} alt="Коллекционное издание" />
                   <div className={styles.details__payment}>
                      <span className={styles.details__price}>399 ₽</span>
                      <MyButton>приобрести</MyButton>
