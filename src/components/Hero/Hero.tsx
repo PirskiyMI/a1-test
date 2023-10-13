45d0bbf966f3863fe21f9aad1f6b78eae1c064a7
Pirskiy Maxim <pirskiymi98@gmail.com>
2023-10-13 12:13:25 +0300

    fixed Hero images

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,19 +3,18 @@ import styles from './styles/Hero.module.scss';
 import HeroDecor from './HeroDecor';
 import Glow from '../shared/Glow/Glow';
 
+import logoImage from './assets/images/hero-logo.png';
+import boxesImage from './assets/images/hero-boxes.png';
+
 const Hero: FC = () => {
    return (
       <section className={styles.hero}>
          <div className={styles.hero__container}>
             <div className={styles.hero__images}>
                <a className={styles.hero__logo} href="#">
-                  <img src="./assets/images/hero-logo.png" alt="Аллоды" />
+                  <img src={logoImage} alt="Аллоды" />
                </a>
-               <img
-                  className={styles.hero__image}
-                  src="./assets/images/hero-boxes.png"
-                  alt="Коллекционное издание"
-               />
+               <img className={styles.hero__image} src={boxesImage} alt="Коллекционное издание" />
             </div>
             <h1 className={styles.hero__title}>Коллекционные издания</h1>
             <p className={styles.hero__text}>
